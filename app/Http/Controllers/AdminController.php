aae4dccbfecf414c7afc445d51cc7c2f6e76405a
Luciano Elly <lucianoelly123@gmail.com>
2025-02-18 22:29:28 -0300

    refatorado dashboard do admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,6 +13,6 @@ class AdminController extends Controller
      */
     public function dashboard()
     {
-        return view('adminDashboard');
+        return view('admin.dashboard');
     }
 }
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.default', [
+    'model_title' => 'Dashboard',
+    'page_title' => 'Dashboard Admin',
+    'search_route' => 'admin.livros.index',
+])
+@section('content')
+    <div class="text-white display-4">
+
+        <div class="row mb-3 gap-3">
+            <a class="col text-center py-3" href="{{ route('admin.livros.index') }}"
+                style="background-color: rgb(17, 75, 95);">
+                <i class="fa-solid fa-pencil"></i>
+                <span>LIVROS</span>
+            </a>
+    
+            <a class="col text-center py-3" href="{{ route('admin.colecoes.index') }}"
+                style="background-color: rgb(2, 128, 144);">
+                <i class="fa-solid fa-pencil"></i>
+                <span>COLEÇÕES</span>
+            </a>
+        </div>
+
+        <div class="row mb-3 gap-3">
+            <a class="col text-center py-3" href="{{ route('admin.generos.index') }}"
+                style="background-color: rgb(190, 110, 70);">
+                <i class="fa-solid fa-pencil"></i>
+                <span>GÊNEROS</span>
+            </a>
+    
+            <a class="col text-center py-3" href="{{ route('admin.editoras.index') }}"
+                style="background-color: rgb(255, 200, 87);">
+                <i class="fa-solid fa-pencil"></i>
+                <span>EDITORAS</span>
+            </a>
+        </div>
+
+        <hr class="mb-3 text-black">
+
+        <div class="row mb-3 gap-3">
+            <a class="col text-center py-3" href="{{ route('admin.relatorios.estoque.page') }}"
+                style="background-color: rgb(175, 27, 63);">
+                <i class="fa-solid fa-square-poll-horizontal"></i>
+                <span>QTD. ESTOQUE</span>
+            </a>
+    
+            <a class="col text-center py-3" href="{{ route('admin.relatorios.vendas_periodo.page') }}"
+                style="background-color: rgb(71, 49, 68);">
+                <i class="fa-solid fa-square-poll-horizontal"></i>
+                <span>MAIS VENDIDOS</span>
+            </a>
+        </div>
+
+    </div>
+@stop
